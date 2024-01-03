94a27e6d6399d4797cc52f7d701fe1152d5fd45c
Cerae2 <superuser8162022@gmail.com>
2024-01-03 18:28:30 +0800

    added 5 files

--- itsoproject/industrialdesignforms/models.py
+++ itsoproject/industrialdesignforms/models.py
@@ -1,9 +1,22 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
-class IndustrialFile(models.Model):
+def validate_pdf(value):
+    if not value.name.endswith('.pdf'):
+        raise ValidationError('File must be PDF.')
 
+class IndustrialFile(models.Model):
     title = models.CharField(max_length=255)
-    file = models.FileField(upload_to='industrialdesignfiles/')
-  
+    file1 = models.FileField(upload_to='industrialdesignfiles/', blank=True, null=True, validators=[validate_pdf])
+    file2 = models.FileField(upload_to='industrialdesignfiles/', blank=True, null=True, validators=[validate_pdf])
+    file3 = models.FileField(upload_to='industrialdesignfiles/', blank=True, null=True, validators=[validate_pdf])
+    file4 = models.FileField(upload_to='industrialdesignfiles/', blank=True, null=True, validators=[validate_pdf])
+    file5 = models.FileField(upload_to='industrialdesignfiles/', blank=True, null=True, validators=[validate_pdf])
+    
+    def clean(self):
+        # Ensure at least one file (file1 to file4) is provided
+        if not (self.file1 or self.file2 or self.file3 or self.file4):
+            raise ValidationError('At least one file (file1 to file4) is required.')
+
     def __str__(self):
         return self.title
