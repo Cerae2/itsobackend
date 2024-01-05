078202ba6f189855af33970da665ffa13774ed0d
Cerae2 <superuser8162022@gmail.com>
2024-01-05 23:08:40 +0800

    added 5 form with revised

--- itsoproject/utilitymodelsform/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UtilitymodelsformConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'utilitymodelsform'
--- itsoproject/utilitymodelsform/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import UtilityFile
-
-class UtilityFileSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = UtilityFile
-        fields = '__all__'
